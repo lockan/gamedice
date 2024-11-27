ad5dc4d559304264e14e15a7d6fe8d72cce600b7
Andrew Lockhart <andrew.lockhart@sjrb.ca>
2024-11-26 16:03:50 -0800

    Started implementing die rolls and tests

--- gamedice.go
+++ gamedice.go
@@ -1,7 +1,43 @@
 package gamedice
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	rand "math/rand/v2"
+)
 
-func Test() {
-	fmt.Sprintf("GoDice!")
+// Bad Input Handler (e.g. if 0 is provided for a uint64N call.)
+func badInputHandler(i any) uint64 {
+	rec := recover()
+	var result uint64
+	if rec != nil {
+		fmt.Printf("Handled bad input value: %d", i)
+		result = 0
+	} else {
+		result = 1
+	}
+	return result
+}
+
+// Roll dN die
+func Roll(sides uint64) uint64 {
+	defer badInputHandler(sides)
+	result := rand.Uint64N(sides)
+
+	if result >= math.MaxUint64 {
+		return math.MaxUint64
+	} else {
+		return result + 1
+	}
+}
+
+//Roll NdM -
+
+//Parse Roll String in format XdN +/- Y
+
+// ===== misc private test functions
+func forcedResultRoll(sides uint64, forced uint64) uint64 {
+	result := Roll(sides)
+	result = forced
+	return result
 }
---
+++ gamedice_test.go
@@ -0,0 +1,49 @@
+package gamedice
+
+import (
+	"math"
+	"reflect"
+	"testing"
+)
+
+func TestRoll(t *testing.T) {
+	testroll := 1
+	t.Logf("Input is %d", testroll)
+	roll := Roll(uint64(testroll))
+	t.Logf("Roll is %d", roll)
+	var u uint64
+	if reflect.TypeOf(u) != reflect.TypeOf(roll) {
+		t.Log("Result type is not uint64")
+		t.Fail()
+	}
+
+	if roll <= 0 {
+		t.Log("Result is <= 0")
+		t.Fail()
+	}
+
+	if roll > math.MaxUint64 {
+		t.Log("Result greater than max Uint64")
+		t.Fail()
+	}
+}
+
+func TestZeroInput(t *testing.T) {
+	testroll := 0
+	t.Logf("Input is %d", testroll)
+	roll := Roll(uint64(testroll))
+	t.Logf("Roll is %d", roll)
+	if 0 != roll {
+		t.Fail()
+	}
+}
+
+func TestForcedResultRoll(t *testing.T) {
+	testroll := 1
+	t.Logf("Input is %d", testroll)
+	roll := forcedResultRoll(uint64(testroll), math.MaxUint64)
+	t.Logf("Roll is %d", roll)
+	if roll != math.MaxUint64 {
+		t.Fail()
+	}
+}
